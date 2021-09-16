9b4f3106782742ea7af6a18f9024a27909ca6394
AileenB17 <aileen.bagtas@gmail.com>
2021-09-16 18:58:46 +0800

    Sec 4#19: Modeling Our Data

---
+++ backend/models/productModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true }, //individual rating unlike below rating w/c comprises average of all the review ratings
+    comment: { type: String, required: true },
+    // user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' }, //associate the user with the review
+  },
+  { timestamps: true }
+)
+
+const productSchema = mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    brand: { type: String, required: true },
+    category: { type: String, required: true },
+    description: { type: String, required: true },
+    reviews: [reviewSchema],
+    rating: { type: Number, required: true, default: 0 },
+    numReviews: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export const Product = mongoose.model('Product', productSchema)
