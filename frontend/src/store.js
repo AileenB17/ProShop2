c71a016c71383dd018fb920ed634519c36774739
AileenB17 <aileen.bagtas@gmail.com>
2021-09-18 13:51:37 +0800

    Sec 5#27: Create a Redux Store

---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({})
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
+
+// 'store' will then be imported in the main root file which is the index.js.
+// thru the imported store that wrapped App.js, our app can now access all the data and states.
