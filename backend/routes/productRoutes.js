ebb0982cedd7ce7f67361a8ac4a346f4051bc35c
AileenB17 <aileen.bagtas@gmail.com>
2021-09-29 13:37:31 +0800

    Sec 12#75: Create & Update Product Endpoints

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,13 +4,20 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').get(getProducts)
+//To get product list | and post created product (admin user's access only)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
 //To get, delete and update product for admin user's access only
 //any route with '/:id', make sure to put below the same route group because if not, anything we'll put after '/', will be treated as an :id
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 export default router
